b95f6a514d881ea097d79614e4de0b2ec1d94ed4
Neha1659 <nehareddyanthireddy@gmail.com>
2023-03-05 20:57:34 +0530

    Add:Flip the Coin and display if its win or tie and by how much

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,24 +2,30 @@ isHead=1;
 isTail=2;
 headCount=0;
 tailCount=0;
-for ((i=1; i<=10; i++))
+for ((i=1; i<=7; i++))
 do
- coinFace=$((1+RANDOM%2));
-   case $coinFace in
+  for ((j=1; j<=6;j++))
+  do
+   coinFace=$((1+RANDOM%2))
+    case $coinFace in
           $isHead)
           headCount=$(($headCount+1))
-         
           ;;
           $isTail)
           tailCount=$(($tailCount+1))
- 
           ;;
-   esac
-         
-         
-done
-       
+    esac
+  done        
+done       
        echo "Head won $headCount times"       
-       
        echo "Tail won $tailCount times"
 
+       if [ $headCount -gt $tailCount ]
+       then 
+            echo "Head won by $(($headCount-$tailCount)) times"
+       elif [ $headCount -lt $tailCount ]
+       then
+            echo "Tail won by $(($tailCount-$headCount)) times"
+       else
+            echo "Head and Tail won equal times"
+       fi
